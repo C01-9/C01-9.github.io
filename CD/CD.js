690490e6d4a72cf03f9fea8845caa67abd57d594
C01-9 <JanOfi2010@proton.me>
2025-11-22 14:46:34 +0100

    CD lightbox
    made the lightbox work

--- CD/CD.js
+++ CD/CD.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const lightbox = document.getElementById("lightbox");
-  const lightboxImg = document.getElementById("lightbox-img");
   const closeBtn = document.getElementById("lightbox-close");
+  const lightboxP = document.getElementById("lightbox-p");
 
   // Add click event to all images
+  /*
   document.querySelectorAll(".grid-box img").forEach((img) => {
     img.addEventListener("click", () => {
       lightboxImg.src = img.src;
       lightbox.classList.remove("hidden");
     });
   });
+  */
 
+document.querySelectorAll(".grid-box").forEach((div) => {
+    div.addEventListener("click", () => {
+      lightboxP.innerHTML = div.innerHTML;
+      lightbox.classList.remove("hidden");
+    });
+  });
+ 
   // Close lightbox when the close button is clicked
   closeBtn.addEventListener("click", () => {
     lightbox.classList.add("hidden");
@@ -25,3 +34,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
