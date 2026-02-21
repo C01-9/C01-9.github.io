41037e43495030e90d9f7bed587533aca7b93468
C01-9 <JanOfi2010@proton.me>
2026-02-21 18:38:44 +0100

    Update experiments.js
    wait for sound end
    btw i stole the code from https://www.dingusland.fun/

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -1,7 +1,14 @@
 var audio = new Audio('/../src/audio/mario_paint_cat_meow.mp3');
 
 function playsound(event) { 
+    event.preventDefault(); // Prevent the default link action
+    var link = $(this).attr('href'); // Get the link's href
+
     audio.play(event); 
+
+    audio.onended = function () {
+        window.location.href = link;
+    };
 } 
 
 function pausesound(event) { 
