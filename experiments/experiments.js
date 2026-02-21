68b14b3245907a6ddcf62a76525f5a0d402bd24b
C01-9 <JanOfi2010@proton.me>
2026-02-21 15:12:09 +0100

    some more audio stuff
    i wanna go home

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -1,10 +1,15 @@
 var audio = document.getElementById("myAudio"); 
 
-function playsound() { 
-    var audio = document.getElementById("myAudio"); 
+function playsound(event) { 
     audio.play(); 
 } 
 
-function pausesound() { 
+function pausesound(event) { 
   audio.pause(); 
 } 
+
+
+/* event.preventDefault(); // Prevent the default link action
+var link = $(this).attr('href'); // Get the link's href
+
+clickSound.play(); */
